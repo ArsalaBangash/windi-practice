8358ad31d6658e492e3a8fc60e0a0804ac6f7940
Arsala Grey <arsala@grey.software>
2021-11-13 19:47:39 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-screen" id="app">
+    <philosophy-hero />
+  </div>
+</template>
+
+<script lang="ts">
+import PhilosophyHero from "./components/PhilosophyHero.vue";
+
+export default {
+  components: { PhilosophyHero },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PhilosophyHero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mx-auto bg-cover w-1298px h-451px" id="philosophy-hero-container">
+    <hero-cta /> <hero-cta />
+  </div>
+</template>
+
+<script>
+import HeroCta from "./HeroCta.vue";
+
+export default {
+  components: { HeroCta },
+};
+</script>
+
+<style>
+#philosophy-hero-container {
+  background-image: url(./philosophy-hero.avif);
+  border-radius: 16px;
+}
+</style>
